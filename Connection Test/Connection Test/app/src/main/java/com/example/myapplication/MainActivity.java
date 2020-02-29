b8bf16477f0c6b7f96a202b9e405cdccd55fb0b5
Rohithkumard <rohithkumard619@gmail.com>
2020-02-29 10:17:21 +0530

    Merge branch 'master' of https://github.com/sachin-designs/connections.git into DEV_TEST
    # Conflicts:
    #	.gitignore
    #	.idea/codeStyles/Project.xml
    #	.idea/gradle.xml
    #	.idea/misc.xml
    #	.idea/runConfigurations.xml
    #	Application_test/.gitignore
    #	Application_test/.idea/codeStyles/Project.xml
    #	Application_test/.idea/gradle.xml
    #	Application_test/.idea/misc.xml
    #	Application_test/.idea/runConfigurations.xml
    #	Application_test/app/.gitignore
    #	Application_test/app/build.gradle
    #	Application_test/app/google-services.json
    #	Application_test/app/proguard-rules.pro
    #	Application_test/app/src/androidTest/java/com/design/firebase_register/ExampleInstrumentedTest.java
    #	Application_test/app/src/main/AndroidManifest.xml
    #	Application_test/app/src/main/java/com/design/firebase_register/UserDetailes.java
    #	Application_test/app/src/main/res/drawable-v24/ic_launcher_foreground.xml
    #	Application_test/app/src/main/res/drawable/ic_launcher_background.xml
    #	Application_test/app/src/main/res/layout/activity_main.xml
    #	Application_test/app/src/main/res/mipmap-anydpi-v26/ic_launcher.xml
    #	Application_test/app/src/main/res/mipmap-anydpi-v26/ic_launcher_round.xml
    #	Application_test/app/src/main/res/mipmap-hdpi/ic_launcher.png
    #	Application_test/app/src/main/res/mipmap-hdpi/ic_launcher_round.png
    #	Application_test/app/src/main/res/mipmap-mdpi/ic_launcher.png
    #	Application_test/app/src/main/res/mipmap-mdpi/ic_launcher_round.png
    #	Application_test/app/src/main/res/mipmap-xhdpi/ic_launcher.png
    #	Application_test/app/src/main/res/mipmap-xhdpi/ic_launcher_round.png
    #	Application_test/app/src/main/res/mipmap-xxhdpi/ic_launcher.png
    #	Application_test/app/src/main/res/mipmap-xxhdpi/ic_launcher_round.png
    #	Application_test/app/src/main/res/mipmap-xxxhdpi/ic_launcher.png
    #	Application_test/app/src/main/res/mipmap-xxxhdpi/ic_launcher_round.png
    #	Application_test/app/src/main/res/values/colors.xml
    #	Application_test/app/src/main/res/values/strings.xml
    #	Application_test/app/src/test/java/com/design/firebase_register/ExampleUnitTest.java
    #	Application_test/build.gradle
    #	Application_test/gradle.properties
    #	Application_test/gradle/wrapper/gradle-wrapper.jar
    #	Application_test/gradle/wrapper/gradle-wrapper.properties
    #	Application_test/gradlew
    #	Application_test/gradlew.bat
    #	Application_test/settings.gradle
    #	Connections/app/build.gradle
    #	app/.gitignore
    #	app/build.gradle
    #	app/proguard-rules.pro
    #	app/src/androidTest/java/com/design/firebase_register/ExampleInstrumentedTest.java
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/drawable-v24/ic_launcher_foreground.xml
    #	app/src/main/res/drawable/ic_launcher_background.xml
    #	app/src/main/res/mipmap-anydpi-v26/ic_launcher.xml
    #	app/src/main/res/mipmap-anydpi-v26/ic_launcher_round.xml
    #	app/src/main/res/mipmap-hdpi/ic_launcher.png
    #	app/src/main/res/mipmap-hdpi/ic_launcher_round.png
    #	app/src/main/res/mipmap-mdpi/ic_launcher.png
    #	app/src/main/res/mipmap-mdpi/ic_launcher_round.png
    #	app/src/main/res/mipmap-xhdpi/ic_launcher.png
    #	app/src/main/res/mipmap-xhdpi/ic_launcher_round.png
    #	app/src/main/res/mipmap-xxhdpi/ic_launcher.png
    #	app/src/main/res/mipmap-xxhdpi/ic_launcher_round.png
    #	app/src/main/res/mipmap-xxxhdpi/ic_launcher.png
    #	app/src/main/res/mipmap-xxxhdpi/ic_launcher_round.png
    #	app/src/main/res/values/colors.xml
    #	app/src/test/java/com/design/firebase_register/ExampleUnitTest.java
    #	build.gradle
    #	gradle.properties
    #	gradle/wrapper/gradle-wrapper.jar
    #	gradle/wrapper/gradle-wrapper.properties
    #	gradlew
    #	gradlew.bat

--- Connection Test/Connection Test/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Connection Test/Connection Test/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,10 +12,19 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -31,18 +40,121 @@ public class MainActivity extends AppCompatActivity {
     SharedPreferences sharedpreferences;
     String sEmail;
     String sPassword;
+    GoogleSignInClient mGoogleSignInClient;
+
+    private static final int RC_SIGN_IN = 234;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAuth = FirebaseAuth.getInstance();
+
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        //Now we will attach a click listener to the sign_in_button
+        //and inside onClick() method we are calling the signIn() method that will open
+        //google sign in intent
+        findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn();
+//                Intent intent = new Intent(MainActivity.this, ChatActivity.class);
+//                finish();
+//                startActivity(intent);
+            }
+        });
     }
+
+    //this method is called on click
+    private void signIn() {
+        //getting the google signin intent
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+
+        //starting the activity for result
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+        Log.d("DB","result fo rcsigin"+RC_SIGN_IN);
+
+    }
+//    protected void onStart() {
+//        super.onStart();
+//
+//        //if the user is already signed in
+//        //we will close this activity
+//        //and take the user to profile activity
+//        if (mAuth.getCurrentUser() != null) {
+//            finish();
+//            startActivity(new Intent(this, ChatActivity.class));
+//        }
+//    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d("DB","result for rcsigin2-"+RC_SIGN_IN);
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                Log.d("DB","result for task1-"+task);
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                Log.d("DB","result for gmailaccount-"+account);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.d("DB", "Google sign in failed", e);
+                // ...
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d("DB", "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+               .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("DB", "signInWithCredential:success");
+                            Register();
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("DB", "signInWithCredential:failure", task.getException());
+                            Snackbar.make(findViewById(R.id.username), "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
+
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    public void Register(){
+        Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+        finish();
+        startActivity(intent);
+    }
+
     public void Register(View view){
         Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
         finish();
         startActivity(intent);
-
     }
+
 
     public void signinOperation(View view){
         email = (EditText)findViewById(R.id.username);
