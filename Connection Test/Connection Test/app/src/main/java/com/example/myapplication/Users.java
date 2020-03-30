88d20482c4c5586cdf2cb3a86db9676ab33df638
sachin-designs <sachinsach2814@gmail.com>
2020-03-30 10:40:48 +0530

    Revert "added IMEI conditions"
    This reverts commit 0dbd15e7

--- Connection Test/Connection Test/app/src/main/java/com/example/myapplication/Users.java
+++ Connection Test/Connection Test/app/src/main/java/com/example/myapplication/Users.java
@@ -12,12 +12,11 @@ public class Users implements Serializable {
     public String school;
     public String work;
     public String Uid;
-    public String IMEI;
 
     Users(){
 
     }
-    Users(String username, String password, String phNumber, String email, String place, String school, String work, String uid, String IMEI){
+    Users(String username, String password, String phNumber, String email, String place, String school, String work, String uid){
         this.username = username;
         this.password = password;
         this.phNumber = phNumber;
@@ -26,9 +25,8 @@ public class Users implements Serializable {
         this.school=school;
         this.work=work;
         this.Uid = uid;
-        this.IMEI = IMEI;
     }
-    Users(String username, String phNumber, String email, String place, String school, String work, String uid, String IMEI){
+    Users(String username, String phNumber, String email, String place, String school, String work, String uid){
         this.username = username;
         this.phNumber = phNumber;
         this.email = email;
@@ -36,6 +34,5 @@ public class Users implements Serializable {
         this.school=school;
         this.work=work;
         this.Uid=uid;
-        this.IMEI = IMEI;
     }
 }
